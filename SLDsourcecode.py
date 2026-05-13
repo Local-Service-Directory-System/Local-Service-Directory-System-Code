1bbbd2a5aae0b94d1a904762c6594e7931002b25
ebcascassandrafaith-ux <ebcas.cassandrafaith@gmail.com>
2026-05-13 23:08:45 +0800

    initial pa

---
+++ SLDsourcecode.py
@@ -0,0 +1,9 @@
+try:
+    with open("SLD.txt", "x") as file:
+        print("File Sucesfully Created")
+except FileExistsError:
+    print("File Exist Already")
+
+def addProvider():
+    with open("SLD.txt", "w") as file:
+        providers = input("")
