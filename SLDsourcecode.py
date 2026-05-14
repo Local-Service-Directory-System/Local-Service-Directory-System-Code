e59ce196e6e8867eed326f7097852639a07cdd9f
ebcascassandrafaith-ux <ebcas.cassandrafaith@gmail.com>
2026-05-14 16:54:08 +0800

    push post application erz

--- SLDsourcecode.py
+++ SLDsourcecode.py
@@ -7,47 +7,102 @@ except FileExistsError:
 personInfo = []
 
 def personalInfo():
-    while True:
-        print("Personal Information")
+        print("\nPersonal Information")
+        firstName = input("Enter First Name: ")
+        
+        lastName = input("Enter Last Name: ")
+        
+        while True:
+            email = input("Enter email: ")
+            if "@" in email and "." in email:
+                break
+            else:
+                print("Invalid email. Please try again.")
+                continue
+
+        while True:
+            try:
+                contact = int(input("Enter contact number: "))
+            except:
+                print("Please enter a valid contact number.")
+                continue
+        while True:
+            try: 
+                dateBirth = input("Enter Birthday (mm/dd/yyyy): ")
+            except:
+                print("Please enter a valid birthdate.")
+                continue
+
+        gender = input("Enter gender (Male/Female): ")
+
+#clear screen
+#------------------------------------------------------------
+        print("\nLocation and Professional Info")
+        city = input("Enter City: ")
+
+        province = input("Enter province: ")
+        while True:
+            try: 
+                zipCode = int(input("Enter Zip Code (####): "))
+            except:
+                print("Please enter a valid zip code.")
+                continue
+
+        profTitle = input("Enter Professional Title (ex. Electrician): ")
+        while True:
+            try: 
+                hourRate = int(input("Enter your hourly rate: "))
+            except:
+                print("Enter valid hourly rate.")
+#clear screen
+#----------------------------------------------------------------
+        print("\n--=Work Experience (in a Company)=--")
+        print("Enter N/A if not applicable\n")
+
+        jobTitle = input("Enter job title (company position or): ")
+        companyName = input("Enter company name: ")
+        while True:
+            try: 
+                dateStart = int(input("Enter date started (Year): "))
+            except:
+                print("Please enter a valid year")
+                continue
+        while True:
+            try:
+                dateEnded = int(input("Enter date ended (Year): "))
+            except:
+                print("Please enter a valid date.")
+
+#clear screen
+
+        personInfos = {
+            "firstName": firstName,
+            "lastName": lastName,
+            "email": email,
+            "contact": contact,
+            "dateBirth": dateBirth,
+            "gender": gender,
+            "city": city,
+            "province": province,
+            "zipCode": zipCode,
+            "profTitle": profTitle,
+            "hourRate": hourRate,
+            "jobTitle": jobTitle,
+            "companyName": companyName,
+            "dateStart": dateStart,   
+            "dateEnded": dateEnded
+        }
         try:
-            firstName = input("Enter First Name: ")
+            personInfo.append(personInfos)
+
+            with open("SLD.txt", "a") as file:
+                file.write(str(personInfos) + "\n")
+
+            print("Information saved succesfully")
         except:
-            print("Letters Only. Please Try Again!")
-            continue
-        try:
-            lastName = input("Enter Last Name: ")
-        except:
-            print("Letters Only. Please Try Again!")
-            continue
-        email = input("Enter email: ")
-        try:
-            contact = input("Enter contact number: ")
-        except:
-            print("Numbers Only. Please try AGain!")
-            continue
-        dateBirth = input("Enter Birthday (mm/dd/yyyy): ")
-        try:
-            gender = input("Enter gender (Male/Female): ")
-        except:
-            print("Male of Female inputs only. Please Try Again!")
-            continue
+            print("An error occured.")
+            
 
+        return
 
-
-
-    personInfos = {
-        "firstName" = firstName,
-        "lastName" = lastName,
-        "email" = email,
-        "service" = service,
-        "contact" = contact,
-        "dateBirth" = dateBirth,
-        "gender" = gender
-    }
-
-    personInfo.append(personInfos)
-
-    with open("SLD.txt", "a") as file:
-        file.write(str(provider) + "\n")
-
-    print("Provi")
+personalInfo()
